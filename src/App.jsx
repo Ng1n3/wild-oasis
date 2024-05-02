6c78608624736f8b3717a42dfcc46070086b869b
ng1n3 <code.with.muyiwa@gmail.com>
2024-05-02 15:00:44 +0100

    upload basefile, setup styled components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button `
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border:none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`
+
+function App() {
+  return <div>
+    <H1>The Wild Oasis</H1>
+    <Button>Sign up</Button>
+  </div>;
+}
+
+export default App;
