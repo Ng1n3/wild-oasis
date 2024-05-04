821b51bfa9bd3c909ef843978b9b4499f17dd4e8
ng1n3 <code.with.muyiwa@gmail.com>
2024-05-04 11:55:34 +0100

    using react-hot-toast for notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from "../../utils/helpers";
 import PropTypes from "prop-types";
 import { deleteCabin } from "../../services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {toast} from 'react-hot-toast'
 
 const TableRow = styled.div`
   display: grid;
@@ -58,12 +59,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("successfully deleted");
+      toast.success("successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabin"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
